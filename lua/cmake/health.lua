b485aaf0284a7377f36c2f967c90b3cddf0418d6
kristian.mostrom <kristian.mostrom@protonmail.com>
2025-10-08 01:20:41 +0200

    added cmake binary check to health module

--- lua/cmake/health.lua
+++ lua/cmake/health.lua
@@ -1,5 +1,6 @@
 
 local api = require("cmake.api")
+local config = require("cmake.config")
 
 local M = {}
 
@@ -12,12 +13,26 @@ function M.check()
 
     if not api.is_setup() then
         health.error(
-            string.format("%s is not set up. Please configuration to run require(\"cmake\").setup() to configure the plugin.", api.PLUGIN_NAME)
+            string.format(" - %s is not set up. Make sure to run require(\"cmake\").setup() to configure the plugin.", api.PLUGIN_NAME)
         )
         return
     else
         health.ok(" - Plugin is configured")
     end
+
+    if config.cmake_executable_path == nil or config.cmake_executable_path == "" then
+        health.error(" - CMake executable path is not set. Please set cmake_executable_path in the configuration.")
+    else
+        local handle = io.popen(string.format('"%s" --version', config.cmake_executable_path))
+        local result = handle:read("*a")
+        handle:close()
+        if result and result:match("cmake version") then
+            health.ok(string.format(" - Found CMake executable at '%s'", config.cmake_executable_path))
+        else
+            health.error(string.format(" - CMake executable not found at '%s'. Make sure the path is correct.", config.cmake_executable_path))
+        end
+    end
+
 end
 
 return M
